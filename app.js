46bdf5969cfe616856edeb43e86390cebe56f64d
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-04-11 15:27:51 +0200

    api added

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ import express from 'express'
 import compression from 'compression'
 import http from 'http'
 import module from 'path'
+import { graphql } from '@octokit/graphql'
 import { Server } from "socket.io"
 import ejs from 'ejs'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const server = http.createServer(app)
 const io = new Server(server);
+const graphqlAuth = graphql.defaults({
+  headers: { authorization: 'token ' +  process.env.API_KEY},
+})
 
 // const hostname = '127.0.0.1';
 const port = process.env.PORT || 5500
@@ -20,6 +27,8 @@ io.on('connection', socket => {
     console.log('user disconnected')
   })
 })
+
+
 
 app
   .use(compression())
@@ -33,7 +42,25 @@ app
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .get('/', (req, res) => {
-  res.render('index')
+
+  graphqlAuth(`{
+      user(login: "jody29") {
+        issues(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
+          edges {
+            node {
+              bodyText
+            }
+          }
+        }
+      }
+    }`).then(data => {
+
+      console.log(data.user.issues.edges)
+      res.render('index', {
+        issues: data.user.issues.edges
+      })
+    })
+  
 });
 
 server.listen(port, () => {

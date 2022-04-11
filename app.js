b12ce277de01cde9aeb1c02cf7d9f42e0705bc2a
joeribouwman25 <joeri-bouwman@live.nl>
2022-04-11 12:25:56 +0200

    installed socket

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 import express from 'express'
 import compression from 'compression'
+import http from 'http'
+import module from 'path'
+import { Server } from "socket.io"
 import ejs from 'ejs'
 
 const app = express();
+const server = http.createServer(app)
+const io = new Server(server);
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
 const port = process.env.PORT || 5500;
 
-app.use(compression());
-app.use(/.*-[0-9a-f]{10}\..*/, (req, res, next) => {
-    res.setHeader('Cache-Control', 'max-age=365000000, immutable');
-    next();
-});
+io.on('connection', socket => {
+  console.log('a user connected')
+})
 
-app.set('view engine', 'ejs');
+app
+  .use(compression())
+  .use(/.*-[0-9a-f]{10}\..*/, (req, res, next) => {
+    res.setHeader('Cache-Control', 'max-age=365000000, immutable')
+    next()
+})
 
-app.use(express.static('static'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
+  .set('view engine', 'ejs')
+  .use(express.static('static'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("App is running on port " + port);
-  });
+});

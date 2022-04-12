092487ff7493a7d60d810721c1e29cd75a3c8c9a
Remco <remco.kuckelkorn@soverin.net>
2022-04-12 10:54:10 +0200

    initialise sessions

--- app.js
+++ app.js
@@ -8,35 +8,8 @@ const express = require('express')
 const ejs = require('ejs')
 const compression = require('compression')
 const passport = require('passport')
-const GitHubStrategy = require("passport-github2").Strategy
 require('dotenv').config();
-
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:5500/github/callback",
-},
-function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function () {
-    
-    return done(null, profile);
-  })
-}
-))
-
-
-
-require('dotenv').config()
-
-
+require('./server/modules/passportModule.js')(passport);
 
 const app = express();
 const server = http.createServer(app)
@@ -50,8 +23,14 @@ app
   .use(/.*-[0-9a-f]{10}\..*/, (req, res, next) => {
     res.setHeader('Cache-Control', 'max-age=365000000, immutable')
     next()
-})
+  })
+  .use(session({ 
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: true
+  }))
   .use(passport.initialize())
+  .use(passport.session())
   .set('view engine', 'ejs')
   .set('views', 'server/views')
   .use(express.static('public'))
@@ -63,3 +42,24 @@ app
   server.listen(port, () => {
     console.log("App is running on port " + port)
 })
+
+function logout(req, res ){
+  req.logout();
+  req.session.destroy(); 
+  res.redirect('/');
+}
+
+// Setup session to use res.locals for the user
+function saveLocal (req, res, next){
+  res.locals.user = req.user || null;
+  next();
+}
+
+// Check if user is logged in 
+function loggedIn (req, res, next){
+  if (req.isAuthenticated()){
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+}
